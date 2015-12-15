30068d8aa91f5d404ff1f8d456eda34060ed871d
LiquidSmokeX64 <liquidsmokex64@gmail.com>
2015-12-15 00:38:02 -0800

    Odexing

--- Android.mk
+++ Android.mk
@@ -16,6 +16,9 @@ LOCAL_PACKAGE_NAME := CMBugReport
 
 LOCAL_PROGUARD_ENABLED := disabled
 
+# Enable dexpreopt to speed boot time
+WITH_DEXPREOPT := true
+
 include $(BUILD_PACKAGE)
 
 # build tests
